57231375b3f3b16fe3a636bede7c526152cb1259
aishwarya202005 <aishwarya.s@students.iiit.ac.in>
2019-03-05 17:52:32 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import numpy as np
+
+
+print 'hey its me'
+c = np.array([[3.,4], [5.,6], [6.,7]])
+step = tf.reduce_mean(c, 1)                                                                                 
+with tf.Session() as sess:
+    print(sess.run(step))
